2cb6fad6ea4187d4152c61b7f731ad5b17b4c675
Jean-Charles GOULEAU <jean-charles.gouleau@savoye.com>
2023-10-26 08:58:14 +0200

    test acheive

---
+++ BitCounting.spec.ts
@@ -0,0 +1,16 @@
+const countBits = (number: number) => Array.from(number.toString(2)).reduce((a, b) => Number(a) + Number(b) , 0);
+
+describe('test kata counting', function () {
+    it("compter les bit dans integer", function () {
+
+        expect(countBits(0)).toBe(0);
+        expect(countBits(4)).toBe(1);
+        expect(countBits(7)).toBe(3);
+        expect(countBits(9)).toBe(2);
+        expect(countBits(10)).toBe(2);
+
+
+    });
+
+
+});
