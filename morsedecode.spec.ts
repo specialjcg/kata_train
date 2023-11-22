b1d282e59f5cdb4a46aa2b16141e5c9dff9c441b
Jean-Charles GOULEAU <jean-charles.gouleau@savoye.com>
2023-11-22 23:10:13 +0100

    second commit                                   //

--- morsedecode.spec.ts
+++ morsedecode.spec.ts
@@ -1,15 +1,84 @@
 import {describe, expect, it} from "vitest";
 
+type MorseDictionary = {
+    [key: string]: string;
+};
+
+const MORSE_CODE: MorseDictionary = {
+    'A': '.-',
+    'B': '-...',
+    'C': '-.-.',
+    'D': '-..',
+    'E': '.',
+    'F': '..-.',
+    'G': '--.',
+    'H': '....',
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..',
+    '0': '-----',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    '.': '.-.-.-',
+    ',': '--..--',
+    '?': '..--..',
+    '\'': '.----.',
+    '!': '-.-.--',
+    '/': '-..-.',
+    '(': '-.--.',
+    ')': '-.--.-',
+    '&': '.-...',
+    ':': '---...',
+    ';': '-.-.-.',
+    '=': '-...-',
+    '+': '.-.-.',
+    '-': '-....-',
+    '_': '..--.-',
+    '"': '.-..-.',
+    '$': '...-..-',
+    '@': '.--.-.',
+    ' ': ' '
+};
+type InverseMorseDictionary = {
+    [key: string]: string;
+};
+// Create the inverse dictionary
+const inverseMorseCodeDictionary: InverseMorseDictionary = {};
+Object.entries(MORSE_CODE).forEach(([key, value]) => {
+    inverseMorseCodeDictionary[value] = key;
+});
+
+const decodeMorse = (s: string):string => {
+    return s.split(' ')
+        .map(morseChar => inverseMorseCodeDictionary[morseChar] || ' ')
+        .join('').replace('  ',' ');};
 
 describe('test kata morse decode', function () {
-    it("compter les bit dans integer", function () {
+    it("decode message morse", function () {
 
-        expect(countBits(0)).toBe(0);
-        expect(countBits(4)).toBe(1);
-        expect(countBits(7)).toBe(3);
-        expect(countBits(9)).toBe(2);
-        expect(countBits(10)).toBe(2);
-
+        expect(decodeMorse('.... . -.--   .--- ..- -.. .')).toStrictEqual( 'HEY JUDE')
 
     });
 
